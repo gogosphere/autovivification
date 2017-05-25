c443b69f9456e43cd64b72f2bc2453659891424f
GoGoSphere <gogosphere@gmail.com>
2017-05-25 14:15:52 -0500

    initial

---
+++ autoviv.py
@@ -0,0 +1,26 @@
+class AutoVivification(dict):
+    """Implementation of perl's autovivification feature."""
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
+
+h = AutoVivification()
+
+for key in lists:
+    a = key[4]
+    b = key[0]
+    c = key[1]
+    d = key[2]
+    e = key[3]
+    if not h[a][b][c][d]:
+        h[a][b][c][d] = int()
+    h[a][b][c][d] += 1 
+
+for x in h:
+    for y in h[x]:
+        for z in h[x][y]:
+            for a in h[x][y][z]:
+                print  x, y, z, a, h[x][y][z][a]
